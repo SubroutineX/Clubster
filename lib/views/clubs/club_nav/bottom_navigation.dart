30a3932c113b2b31c0a655947bccc5c855137e24
Atharvakulkarni31 <55599995+Atharvakulkarni31@users.noreply.github.com>
2020-12-11 20:26:21 +0530

    navigation

--- lib/views/clubs/club_nav/bottom_navigation.dart
+++ lib/views/clubs/club_nav/bottom_navigation.dart
@@ -1,5 +1,7 @@
 import "package:flutter/material.dart";
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:workflow/models/clubs.dart';
+import 'package:workflow/views/clubs/club_nav/create_club.dart';
 import 'package:workflow/views/clubs/club_nav/custom_bottomNav.dart';
 import 'package:workflow/views/styles/colors.dart';
 import 'package:workflow/views/styles/styles.dart';
@@ -13,9 +15,15 @@ enum _SelectedTab { home, likes, search, profile, clash }
 
 class _CustomBottomNavState extends State<CustomBottomNav> {
   var _selectedTab = _SelectedTab.home;
+  int _currentIndex = 0;
+
+  final List<Widget> _children = [
+    CreateCulb(),
+  ];
 
   void _handleIndexChanged(int i) {
     setState(() {
+      _currentIndex = i;
       _selectedTab = _SelectedTab.values[i];
       print(MediaQuery.of(context).size.width);
     });
@@ -25,6 +33,7 @@ class _CustomBottomNavState extends State<CustomBottomNav> {
   Widget build(BuildContext context) {
     var deviceDimensions = MediaQuery.of(context).size;
     return Container(
+      body: _children[_currentIndex],
       width: deviceDimensions.width,
       padding: EdgeInsets.symmetric(vertical: 5),
       decoration: BoxDecoration(
@@ -35,6 +44,8 @@ class _CustomBottomNavState extends State<CustomBottomNav> {
       child: CustomBottomBar(
         currentIndex: _SelectedTab.values.indexOf(_selectedTab),
         onTap: _handleIndexChanged,
+        //  onTap: onTappedBar,
+        // currentIndex: _currentIndex,
         items: [
           CustomBottomBarItem(
             icon: Icon(Icons.group_outlined),
