ecfc28f3df067f486db538d55893915e261bc6d3
Abhijeet18t <58684531+Abhijeet18t@users.noreply.github.com>
2020-12-20 15:03:34 +0530

    clubs form combined

--- Backend/controllers/register_club.js
+++ Backend/controllers/register_club.js
@@ -1,25 +1,74 @@
+var multer = require("multer");
+var path = require("path");
 const mongoose = require("mongoose");
 const clubs = require("../models/clubs_model");
 
 module.exports = (req, res) => {
     try {
-        var clubsModel = new clubs({
-            _id: mongoose.Types.ObjectId(),
-            clubName: req.body.clubName,
-            genre: req.body.genre,
-            description: req.body.description,
-            status: req.body.status,
-            memberLimit: req.body.memberLimit,
-            members: req.body.members
+        fileName = "filename.jpg";
+        var storage = multer.diskStorage({
+            destination: function (req, file, cb) {
+                // Uploads is the Upload_folder_name
+                cb(null, "uploads");
+            },
+            filename: function (req, file, cb) {
+                cb(null, fileName);
+            },
         });
-        // clubsModel.members.push(req.body.members);
-        clubsModel.save();
-        res.status(200).json("club created");
+        const maxSize = 1 * 1000 * 1000;
 
+        var upload = multer({
+            storage: storage,
+            limits: { fileSize: maxSize },
+            fileFilter: function (req, file, cb) {
+                // Set the filetypes, it is optional
+                var filetypes = /jpeg|jpg|png/;
+                var mimetype = filetypes.test(file.mimetype);
+
+                var extname = filetypes.test(
+                    path.extname(file.originalname).toLowerCase()
+                );
+
+                if (mimetype && extname) {
+                    return cb(null, true);
+                }
+
+                cb(
+                    "Error: File upload only supports the " +
+                        "following filetypes - " +
+                        filetypes
+                );
+            },
+            // mypic is the name of file attribute
+        }).single("clubImage");
+        // Error MiddleWare for multer file upload, so if any
+        // error occurs, the image would not be uploaded!
+        upload(req, res, function (error) {
+            if (error) {
+                // ERROR occured (here it can be occured due
+                // to uploading image of size greater than
+                // 1MB or uploading different file type)
+                res.status(500).json(error);
+            } else {
+                var clubsModel = new clubs({
+                    _id: mongoose.Types.ObjectId(),
+                    clubName: req.body.clubName,
+                    genre: req.body.genre,
+                    description: req.body.description,
+                    status: req.body.status,
+                    memberLimit: req.body.memberLimit,
+                    members: req.body.members,
+                });
+                // clubsModel.members.push(req.body.members);
+                clubsModel.save();
+                res.status(200).json("club created");
+
+                // SUCCESS, image successfully uploaded
+                // res.status(200).json("Success, Image uploaded!")
+            }
+        });
     } catch (error) {
         console.log(error);
-        res.status(500).json({ error: error });
-
+        res.status(500).json(error.message);
     }
-}
-
+};
--- Backend/server.js
+++ Backend/server.js
@@ -1,39 +1,39 @@
 if (process.env.NODE_ENV !== "production") {
-	require("dotenv").config()
+    require("dotenv").config();
 }
-const express = require("express")
-const app = express()
-const PORT = process.env.PORT || 8000
-const mongoose = require("mongoose")
-const bodyParser = require("body-parser")
-const path = require("path")
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8000;
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.use(express.urlencoded({ extended: false }))
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 //database connection
 var mongoDB =
-	"mongodb+srv://admin:" +
-	process.env.mongoDB +
-	"@cluster0.hed3w.mongodb.net/WorkFlow?retryWrites=true&w=majority"
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+    "mongodb+srv://admin:" +
+    process.env.mongoDB +
+    "@cluster0.hed3w.mongodb.net/WorkFlow?retryWrites=true&w=majority";
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //helpers
-const authenticateToken = require("./helpers/authenticate_token")
+const authenticateToken = require("./helpers/authenticate_token");
 
 //controllers:-
-const registerUser = require("./controllers/register_user.js")
-const registerClub = require("./controllers/register_club.js")
-const uploadClubImage = require("./controllers/upload_club_image")
+const registerUser = require("./controllers/register_user.js");
+const registerClub = require("./controllers/register_club");
 
 //Routes
 app.get("/", authenticateToken, (req, res) => {
-	res.status(200).send("hello")
-})
-app.post("/register", registerUser)
-app.post("/registerClub", authenticateToken, registerClub)
-app.post("/uploadClubImage", authenticateToken, uploadClubImage)
+    // res.render("index.ejs");
+    res.status(200).send("hello");
+});
+app.post("/register", registerUser);
+app.post("/registerClub", authenticateToken, registerClub);
 
-app.listen(PORT)
+app.listen(PORT);
