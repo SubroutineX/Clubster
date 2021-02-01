173a32a14d316c2f1b1afc2e69d7f7a82bec2d4c
Atharvakulkarni31 <55599995+Atharvakulkarni31@users.noreply.github.com>
2021-02-01 23:13:42 +0530

    update

--- lib/views/clubs/clubs_upload/upload_discription.dart
+++ lib/views/clubs/clubs_upload/upload_discription.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:workflow/views/styles/colors.dart';
 
 class Discription extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: red,
       child: Column(
         children: [
           Container(
